5c1cdf7b288a4d26aba275cb5a7697574152a77c
Lucas Sobrinho <lucas.ns.93@hotmail.com>
2025-03-26 14:05:54 -0300

    atualização

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import base64
+import cv2
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from typing import List
+from fastapi.responses import JSONResponse
 import torch
 from torchvision import transforms
 from PIL import Image
@@ -10,8 +13,10 @@ import joblib
 from sklearn.preprocessing import StandardScaler
 from AlzheimerCNN import AlzheimerCNN
 from ClinicalData import ClinicalData
+from gradcam_utils import GradCAM
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.preprocessing import LabelEncoder
+import torch.nn.functional as F
 
 app = FastAPI()
 
@@ -20,19 +25,28 @@ scaler.mean_ = np.array([60, 12, 2, 27, 0.5, 1500, 0.7, 1.2, 5])
 scaler.scale_ = np.array([10, 3, 1, 2, 0.5, 300, 0.1, 0.2, 2])
 
 model_mri = AlzheimerCNN()
-model_mri.load_state_dict(torch.load("alzheimer_mri_model.pth", map_location=torch.device("cpu")))
+model = torch.load("alzheimer_mri_model.pth", map_location=torch.device("cpu"))
+model_mri.load_state_dict(model["model_state"])
+
+class_to_idx = model["class_to_idx"]
+print("Mapeamento de Classes:")
+for idx, class_name in class_to_idx.items():
+    print(f"{idx}: {class_name}")
+
 model_mri.eval()
 
 model_csv = joblib.load('alzheimer_csv_model.joblib')
 scaler_csv = joblib.load('scaler.pkl')
 
-classes = ["MildDemented", "ModerateDemented", "VeryMildDemented", "NonDemented"]
+classes = ["MildImpairment", "ModerateImpairment", "NoImpairment", "VeryMildImpairment"]
 
 transform = transforms.Compose([
     transforms.Resize((128, 128)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5], std=[0.5])
 ])
+
+gradcam = GradCAM(model_mri, model_mri.conv3)
 
 @app.post("/predict")
 async def predict(files: List[UploadFile] = File(...)):
@@ -45,8 +59,10 @@ async def predict(files: List[UploadFile] = File(...)):
         with torch.no_grad():
             output = model_mri(image)
             predicted_class = torch.argmax(output, dim=1).item()
+            probabilities = F.softmax(output, dim=1)
 
-        results[file.filename] = {"classe_predita": classes[predicted_class]}
+        prob_dict = {classes[i]: f"{probabilities[0, i].item() * 100:.2f}%" for i in range(len(classes))}
+        results[file.filename] = {"probabilidades": prob_dict, "classe_predita": classes[predicted_class]}
 
     return results
 
@@ -74,6 +90,32 @@ async def predict_clinical_data(data: List[dict]):
         results.append({"PatientID": record["PatientID"], "PredictedDiagnosis": diagnosis})
 
     return results
+
+
+@app.post("/predict/heat_map")
+async def predict(file: UploadFile = File(...)):
+    # Lê a imagem enviada
+    image_bytes = await file.read()
+
+    # Pré-processa a imagem
+    image_tensor = gradcam.preprocess_image(image_bytes)
+
+    # Gera o mapa de calor e a classe predita
+    overlayed_image, predicted_class, predicted_probability = gradcam.create_heatmap_and_class(image_tensor)
+
+    # Codifica a imagem com o mapa de calor para base64
+    _, img_encoded = cv2.imencode('.jpg', overlayed_image)
+    img_bytes = img_encoded.tobytes()
+    img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+
+    # Gera o JSON com as probabilidades da classe
+    response = {
+        "predicted_class": predicted_class,
+        "predicted_probability": predicted_probability,
+        "heatmap_image": f"data:image/jpeg;base64,{img_base64}" 
+    }
+
+    return JSONResponse(content=response)
 
 
 if __name__ == "__main__":
