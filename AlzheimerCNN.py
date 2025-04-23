5b1b3d4726235b1809019554c654807bff27633a
Lucas Sobrinho <lucas.ns.93@hotmail.com>
2025-04-23 12:59:56 -0300

    merge fixed

--- AlzheimerCNN.py
+++ AlzheimerCNN.py
@@ -7,7 +7,7 @@ class AlzheimerCNN(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((7, 7))  # Makes model work with any input size
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((7, 7))
         self.fc1 = nn.Linear(128 * 7 * 7, 512)
         self.fc2 = nn.Linear(512, num_classes)
         self.dropout = nn.Dropout(0.5)
@@ -16,9 +16,9 @@ class AlzheimerCNN(nn.Module):
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
-        x = self.pool(self.relu(self.conv3(x)))
-        x = self.adaptive_pool(x)  # Outputs 7x7 regardless of input size
-        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = self.relu(self.conv3(x))  # Remover pooling após conv3 para preservar informações
+        x = self.adaptive_pool(x)
+        x = x.view(x.size(0), -1)
         x = self.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
